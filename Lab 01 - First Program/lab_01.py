c6533933a1c59f6ca3f5328a9e8ff158bd6f5382
Rodrigo Islan <rodrigo.islan@edu.uah.es>
2022-02-10 18:15:22 +0100

    Lab 01

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,6 @@
-Hi!
+print('Hello classmates!')
+print('My name is \"Rodrigo\"')
+print('I\nam\nstudying\nComputer Engineering')
+print("""I like practicing sports
+Hanging out with my friends
+Watch TV series""")
